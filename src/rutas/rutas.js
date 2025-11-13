fea18cd5f7b46ea5075a1b9f69302cc71dbf13ca
DianaGabiFlores <diana.gabi.flores@gmail.com>
2025-11-13 09:55:34 -0600

    Ruta módulos

--- src/rutas/rutas.js
+++ src/rutas/rutas.js
@@ -25,7 +25,51 @@ router.get('/familias', async (req, res) => {
   }
 });
 
+router.get('/modulos', async (req, res) => {
+  try {
+    // Recibe los módulos
+    const modulos = await pool.query(
+        'Select * from MODULO;'
+    );
+    
+    // Por cada comentario principal traer los comentarios réplicas
+    const Modulo_Secciones_Promises = modulos[0].map(async modulo => {
+      const id = modulo.ID_Modulo;
+      // Extraer las secciones por cada módulo
+      const secciones = await pool.query(
+        `SELECT S.* FROM SECCION S INNER JOIN MODULO M ON M.ID_Modulo = S.Id_Modulo WHERE M.ID_Modulo = ?;`,
+        [id]
+      );
+      const respuesta_final = await Promise.all(secciones); 
+      return {
+        ...modulo,
+        secciones: respuesta_final[0]
+      };
+    });
+
+    const Modulo_Secciones = await Promise.all(Modulo_Secciones_Promises); 
+    console.log(Modulo_Secciones);
+
+    return res.status(200).json({ // Changed to 200 OK for GET request
+      success: true,
+      message: 'Modulos encontrados',
+      response: Modulo_Secciones
+    });
+
+  }catch(err){
+    console.error('Error al traer los Modulos: ', err);
+    res.status(500).json({
+      success: false,
+      message: 'Error interno del servidor',
+      response: null
+    });
+  }  
+});
+
+// ------------------------------------
 // Ruta para traer palabras que coincidan con la búsqueda
+// Accesible en: /palabras/busqueda
+// ------------------------------------
 router.post('/palabras/busqueda', async (req, res) => {
   console.log(req.body);
   try {
@@ -57,7 +101,10 @@ router.post('/palabras/busqueda', async (req, res) => {
   }  
 });
 
+// ------------------------------------
 // Endpoint para registrar un nuevo usuario si es que aún no esta registrado
+// // Accesible en: /usuario/crear
+// ------------------------------------
 router.post('/usuario/crear', async (req, res) => {
   try {
     const { Correo, Nombre, Nickname, Imagen} = req.body;
@@ -110,20 +157,24 @@ router.post('/usuario/crear', async (req, res) => {
   }
 });
 
+
+// ------------------------------------
 // Ruta para traer todos los comentarios
+// Accesible en: /comentarios/todos
+// ------------------------------------
 router.get('/comentarios/todos', async (req, res) => {
   try {
+    // Recibe todos los comentarios que son principales, y los ordena de forma descendente por ID
     const comentarios = await pool.query(
-        'SELECT C.ID_Comentario, U.Nombre, U.imagen as U_imagen, DATE_FORMAT(C.fecha,  "%d %b %Y") as fecha, C.asunto, C.descripcion, C.video, C.imagen, C.Documento, ( SELECT COUNT(*) FROM COMENTARIO C2 WHERE C2.ID_Comentario_respuesta = C.ID_Comentario ) AS Total_Respuestas FROM Comentario C INNER JOIN USUARIO U ON C.ID_Usuario = U.ID_User WHERE C.ID_Comentario_respuesta IS NULL ORDER BY C.ID_Comentario;'
+        'SELECT C.ID_Comentario, U.Nombre, U.imagen as U_imagen, DATE_FORMAT(C.fecha,  "%d %b %Y") as fecha, C.asunto, C.descripcion, C.video, C.imagen, C.Documento, ( SELECT COUNT(*) FROM COMENTARIO C2 WHERE C2.ID_Comentario_respuesta = C.ID_Comentario ) AS Total_Respuestas FROM Comentario C INNER JOIN USUARIO U ON C.ID_Usuario = U.ID_User WHERE C.ID_Comentario_respuesta IS NULL ORDER BY C.ID_Comentario DESC;'
     );
-
-    console.log(comentarios);
     
+    // Por cada comentario principal traer los comentarios réplicas
     const Comentarios_Replies_Promises = comentarios[0].map(async comentario => {
       const id = comentario.ID_Comentario;
       // Extraer los comentarios respuesta
       const respuestas = await pool.query(
-        `SELECT C.ID_Comentario, U.Nombre, U.imagen as U_imagen, DATE_FORMAT(C.fecha, '%d %b %Y') as fecha, C.asunto, C.descripcion, C.video, C.imagen, C.Documento, 0 AS Total_Respuestas  FROM Comentario C INNER JOIN USUARIO U ON C.ID_Usuario = U.ID_User WHERE C.ID_Comentario_respuesta = ?; `,
+        `SELECT C.ID_Comentario, U.Nombre, U.imagen as U_imagen, DATE_FORMAT(C.fecha, '%d %b %Y') as fecha, C.asunto, C.descripcion, C.video, C.imagen, C.Documento, -1 AS Total_Respuestas  FROM Comentario C INNER JOIN USUARIO U ON C.ID_Usuario = U.ID_User WHERE C.ID_Comentario_respuesta = ? ORDER BY C.ID_Comentario; `,
         [id]
       );
       const respuestas_respuestas = respuestas[0].map(async respuesta => {
@@ -140,6 +191,7 @@ router.get('/comentarios/todos', async (req, res) => {
     });
 
     const Comentarios_Replies = await Promise.all(Comentarios_Replies_Promises); 
+    console.log(Comentarios_Replies);
 
     return res.status(200).json({ // Changed to 200 OK for GET request
       success: true,
@@ -157,22 +209,27 @@ router.get('/comentarios/todos', async (req, res) => {
   }  
 });
 
+// ------------------------------------
 // Endpoint para registrar un nuevo usuario si es que aún no esta registrado
+// Accesible en: /comentario/agregar
+// ------------------------------------
 router.post('/comentario/agregar', async (req, res) => {
   try {
-    //correo o id
+    // id
     const {Id_comentario_padre, Id_usuario, fecha, asunto, descripcion, video, imagen, documento} = req.body;
 
-    console.log(req.body);
+    // Insertar el comentario
     const [rows, fields] = await pool.query(
       ` INSERT INTO COMENTARIO (ID_Usuario, fecha, asunto, descripcion, video, Imagen, Documento, ID_Comentario_respuesta)
        VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
       [Id_usuario, fecha, asunto, descripcion, video, imagen, documento, Id_comentario_padre]
     );
 
+    // Si se inserto correctamente 
     if (rows.affectedRows === 1) {
       console.log(`Row inserted successfully. New ID: ${rows.insertId}`);
     } else {
+      // Si ocurrió un error 
       console.log("Insert operation did not affect the expected number of rows.");
     }
     
